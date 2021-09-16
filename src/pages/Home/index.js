01bbebdd434096d6ebca16a9af3b778de53599e0
Daniel <danielsantoscarneiro0508@gmail.com>
2021-09-16 14:03:41 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import { Title, Container, Paragrafo } from "./styled";
+
+
+export default function Home(){
+    return(
+        <>
+           <Container>
+            <Title>Teste</Title>
+            <Paragrafo>Paragrafo Teste</Paragrafo>
+           </Container>
+        </>
+    )
+}
---
+++ src/pages/Home/styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+    font-family: sans-serif;
+    font-size: 90px;
+`;
+
+export const Container = styled.div`
+    width: 900px;
+    margin: 0 auto;
+    background-color: red;
+`;
+
+export const Paragrafo = styled.p`
+    font-size: 5rem;
+`;
